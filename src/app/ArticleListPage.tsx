8d221637eaa53e266be0ee778cd1ade83a96128a
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2017-02-21 17:35:05 -0600

    Remove commented code

--- src/app/ArticleListPage.tsx
+++ src/app/ArticleListPage.tsx
@@ -23,9 +23,7 @@ import {connect} from "react-redux"
 
 import ArticleList, {StateProps, DispatchProps} from "./ArticleList"
 import {StateModel} from "./state/models"
-// import api from "./state/api"
 import {getPublications, getArticles} from "./state/selectors"
-// import {clearArticlesAction, deleteArticleAction} from "./state/actions"
 
 export interface RouteParams {
     publicationId?: string
@@ -53,91 +51,3 @@ const withReduxConnect = connect<StateProps, DispatchProps, OwnProps>(
 )
 
 export default withReduxConnect(ArticleList)
-
-/*
-class AlreadyLoadingError extends Error {
-    isAlreadyLoadingError: boolean = true
-    static isTypeOf(err: any): err is AlreadyLoadingError {
-        return !!(err as AlreadyLoadingError).isAlreadyLoadingError
-    }
-
-    constructor() {
-        super("Already loading.")
-    }
-}
-
-interface State {
-    articles: ArticleBriefModel[]
-    publications: PublicationModel[]
-    isLoading: boolean
-}
-
-export default class ArticleListPage extends React.PureComponent<OwnProps, State> {
-    state: State = {
-        articles: [],
-        publications: [],
-        isLoading: false,
-    }
-
-    private publicationsPromise: Promise<PublicationModel[]>
-
-    private async reload(publicationId: string = this.props.params.publicationId): Promise<void> {
-        if (this.state.isLoading) {
-            throw new AlreadyLoadingError()
-        }
-
-        if (publicationId) {
-            this.setState({isLoading: true})
-            this.setState({
-                articles: await api.articles.list(publicationId),
-                isLoading: false,
-            })
-        } else {
-            const id = (await this.publicationsPromise)[0].id
-            this.props.router.replace(`/publications/${id}/articles`)
-        }
-    }
-
-    async componentDidMount(): Promise<void> {
-        this.publicationsPromise = api.publications.list()
-        this.setState({publications: await this.publicationsPromise})
-        await this.reload()
-    }
-
-    async componentWillReceiveProps({params: nextParams}: OwnProps): Promise<void> {
-        const {params: currentParams} = this.props
-
-        if (nextParams.publicationId !== currentParams.publicationId) {
-            await this.reload(nextParams.publicationId)
-        }
-    }
-
-    private onArticleDelete = (id: string): void => {
-        api.articles.remove(this.props.params.publicationId, id)
-
-        this.setState(({articles}) => ({
-            articles: articles.filter(article => article.id !== id)
-        }))
-    }
-
-    private onRefresh = async (): Promise<void> => {
-        try {
-            await this.reload()
-        } catch (err) {
-            if (!AlreadyLoadingError.isTypeOf(err)) {
-                throw err
-            }
-        }
-    }
-
-    render(): JSX.Element {
-        const {params} = this.props
-        const {articles, publications, isLoading} = this.state
-        const currentPublication = publications.find(({id}) => id === params.publicationId)
-
-        return <ArticleList
-            {...{articles, publications, isLoading, currentPublication}}
-            onArticleDelete={this.onArticleDelete} onRefresh={this.onRefresh} />
-    }
-}
-*/
